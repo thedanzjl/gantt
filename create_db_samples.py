cf2b7bd037d7810504b04dd9fc34373efb5352ff
Mariia Charikova <m.charikova@innopolis.ru>
2019-04-18 19:42:55 +0300

    Fix

--- create_db_samples.py
+++ create_db_samples.py
@@ -11,7 +11,7 @@ def main():
                    '(name String, start_date String, duration Int32, creation_date Date) '
                    'ENGINE = MergeTree(creation_date, name, 8192)')
 
-    values = [[f'task {i}', str(datetime.today().date()), randint(1, 10), datetime.today()] for i in range(10)]
+    values = [[f'task {i}', str(datetime.today().date()), randint(1, 10), datetime.today().date()] for i in range(10)]
 
     client.execute('insert into Task values ', values)
 
