42c2fb0901c58948992eeb14a6a885655af728a2
concrete13377 <whst13377@gmail.com>
2019-04-23 19:42:43 +0300

    changed gs now it should work better,

--- search.py
+++ search.py
@@ -3,6 +3,8 @@ import time
 
 
 from clickhouse_driver import Client
+from math import sqrt
+
 client = Client('localhost')
 
 
@@ -13,10 +15,19 @@ def search(t, k):
     :param num: number of nearest tasks (if top 3, k = 3)
     also prints elapsed time since it is required search will take less then minute on 1000 records
     """
+    def distance(first, second):
+        # duration and start_time
+        # sum of squared differences
+        a = (datetime.datetime.strptime(first[1], '%Y-%m-%d') - datetime.datetime.strptime(second[0][1], '%Y-%m-%d')) / datetime.timedelta(days=1)
+        b = first[2] - second[0][2]
+        return sqrt(a**2 + b**2)
+
+
 
     start = time.time()
 
     aim = t[0][1]
+
 
     all = client.execute("SELECT * FROM Task")
 
@@ -24,22 +35,29 @@ def search(t, k):
 
     client.execute(''
                    'CREATE TABLE Score '
-                   '(name String, start_date String, score Float32, creation_date Date) '
+                   '(name String, start_date String, duration Int32, score Float32, creation_date Date) '
                    'ENGINE = MergeTree(creation_date, name, 8192)')
 
-    values = [[str(i[0]), str(i[1]), abs((datetime.datetime.strptime(i[1], '%Y-%m-%d') - datetime.datetime.strptime(aim,
-                                                                                                                    '%Y-%m-%d')) / datetime.timedelta(
-        days=1)), datetime.datetime.today().date()] for i in all]
+    values = [
+        [str(i[0]),
+         str(i[1]),
+         i[2],
+         distance(i, t),
+         datetime.datetime.today().date()
+         ]
+        for i in all if i[0] != t[0][0]]
+
     client.execute('insert into Score values ', values)
 
-    result = client.execute('SELECT name, start_date from Score ORDER BY score')
+    result = client.execute('SELECT name, start_date, duration from Score ORDER BY score')
     end = time.time()
     print('search time: ', end - start)
     result = result[:k]
+
     return result
 
 
 if __name__ == '__main__':
-    t = client.execute("SELECT * FROM Task WHERE name = 'task1'")
+    t = client.execute("SELECT * FROM Task WHERE name = 'task 1'")
     print(search(t, 3))
     # [('task 0', '2019-04-22', 10, (), 'description of task 0', 97, datetime.date(2019, 4, 22)),
